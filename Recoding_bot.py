610de4331bab772d7cec0c4e09b8b415c2062538
cart324 <63339005+cart324@users.noreply.github.com>
2024-05-24 02:41:03 +0900

    Update Recoding_bot.py

--- Recoding_bot.py
+++ Recoding_bot.py
@@ -4,12 +4,12 @@ bot = discord.Bot()
 connections = {}
 
 
-@bot.command()
+@bot.command(guild_ids=[312795500757909506, 1242846739434569738])
 async def record(ctx):
     # Check if the author is in a voice channel
     voice = ctx.author.voice
     if not voice:
-        await ctx.respond("You aren't in a voice channel!")
+        await ctx.respond("You aren't in a voice channel!", ephemeral=True)
         return
 
     # Connect to the voice channel the author is in
@@ -20,18 +20,20 @@ async def record(ctx):
         once_done,  # What to do once recording is done
         ctx.channel  # The channel to disconnect from
     )
-    await ctx.respond("Started recording!")
+    await ctx.respond("Started recording!", ephemeral=True)
 
 
-async def once_done(sink: discord.sinks, channel: discord.TextChannel, *args):
-    # Get the recorded users
-    recorded_users = [f"<@{user_id}>" for user_id, audio in sink.audio_data.items()]
+async def once_done(sink, user, *ags):
     await sink.vc.disconnect()  # Disconnect from the voice channel
-    files = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in sink.audio_data.items()]
-    await channel.send(f"Finished recording audio for: {', '.join(recorded_users)}.", files=files)
+    # 녹음된 오디오 데이터를 파일로 저장
+    for user_id, audio in sink.audio_data.items():
+        filename = f"recordings/{user_id}.wav"
+        with open(filename, 'wb') as file:
+            file.write(audio.file.getvalue())
+        await user.send(f"녹음된 오디오 파일을 저장했습니다: {filename}")
 
 
-@bot.command()
+@bot.command(guild_ids=[312795500757909506, 1242846739434569738])
 async def stop_recording(ctx):
     if ctx.guild.id in connections:
         vc = connections[ctx.guild.id]
@@ -39,7 +41,7 @@ async def stop_recording(ctx):
         del connections[ctx.guild.id]  # Remove the guild from the cache
         await ctx.delete()  # Delete the command message
     else:
-        await ctx.respond("I am currently not recording here.")
+        await ctx.respond("I am currently not recording here.", ephemeral=True)
 
 with open('token.txt', 'r') as f:
     token = f.read()
